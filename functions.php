5e5789dd2673480a2e6fe59789dbfea2c05dbd32
Bjørnar Hagen <bjornar@datahjelpen.org>
2016-12-10 16:34:20 +0000

    Added rewrite for work/portfolio items

--- functions.php
+++ functions.php
@@ -3,3 +3,19 @@
  * functions
  * semplice.child.theme
  */
+
+#----------------------------------------#
+# portfolio custom post type             #
+# semplice.theme                         #
+#----------------------------------------#
+
+add_filter( 'register_post_type_args', function ( $args, $post_type ) {
+	// Only target our specific post type
+	if ( 'work' !== $post_type )
+		return $args;
+
+	// Change args
+	$args['rewrite'] = ['slug' => 'innlegg'];
+
+	return $args;
+}, 10, 2 );
